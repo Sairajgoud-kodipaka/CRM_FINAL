e28ee50dbb1534688fe8e40958807db3cb6b90b6
Sairaj K <sairajgoudkodipaka@gmail.com>
2025-10-15 17:46:48 +0530

    15-oct-deploy-2

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -11,17 +11,19 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import telecalling.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django_asgi_app = get_asgi_application()
 
+# Import websocket routes after Django is initialized to avoid AppRegistryNotReady
+from telecalling import routing as telecalling_routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            telecalling.routing.websocket_urlpatterns
+            telecalling_routing.websocket_urlpatterns
         )
     ),
 })
